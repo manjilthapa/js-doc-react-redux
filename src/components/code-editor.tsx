7b008341ea8396bb4353ae2b0e12f8ac2bfd2eb1
manjilthapa <mt@uniscale.com>
2024-08-14 11:54:06 +0200

    Adding prettier to the editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,13 @@
 import Editor, { OnMount } from "@monaco-editor/react"
-import React from "react"
+import React, { useRef } from "react"
+import prettier from "prettier"
+import babelPlugin from "prettier/plugins/babel"
+import esTree from "prettier/plugins/estree"
 
 const CodeEditor: React.FC<{ value: string; onChange: (v: string) => void }> = ({ value, onChange }) => {
+  const editorRef = useRef<any>(null)
   const handleEditorDidMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor
     editor.onDidChangeModelContent(() => {
       onChange(editor.getValue())
     })
@@ -10,24 +15,40 @@ const CodeEditor: React.FC<{ value: string; onChange: (v: string) => void }> = (
     editor.updateOptions({ tabSize: 2 })
   }
 
+  const onFormatClick = async () => {
+    const unformatted = editorRef.current.getValue()
+    const formatted = await prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [babelPlugin, esTree],
+      semi: true,
+      useTabs: false,
+      singleQuote: true,
+    })
+    editorRef.current.setValue(formatted)
+  }
+
   return (
-    <Editor
-      height="500px"
-      value={value}
-      defaultLanguage="javascript"
-      defaultValue="const a = 1;"
-      theme="vs-dark"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      onMount={handleEditorDidMount}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+
+      <Editor
+        height="500px"
+        value={value}
+        defaultLanguage="javascript"
+        defaultValue="const a = 1;"
+        theme="vs-dark"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        onMount={handleEditorDidMount}
+      />
+    </div>
   )
 }
 
